e0546b303433f29c2170dc41be707587e90844aa
way2geek11 <>
2026-06-06 14:37:02 +0000

    Clearing sets

---
+++ mastering_python_sets/clearing_sets/main.py
@@ -0,0 +1,7 @@
+worst_movies = {'Kirk Cameron\'s Saving Christmas', 'The Room', 'Gigli', 'Battlefield Earth', 'Slender Man', 'Catwoman'}
+
+# Write your code here
+worst_movies.clear()
+
+
+print("Updated set:", worst_movies)
