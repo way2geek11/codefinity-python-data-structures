b7e267c188d60e6e073f336465479cf5371e4fb3
way2geek11 <>
2026-06-06 13:09:08 +0000

    Dictionary access

---
+++ mastering_python_dictionaries/dictionary_access/main.py
@@ -0,0 +1,14 @@
+authors_books = {
+    'William Shakespeare': ['Hamlet', 'Macbeth', 'Romeo and Juliet', 'Othello'],
+    'J.K. Rowling': ['Harry Potter and the Sorcerer\'s Stone', 'Harry Potter and the Chamber of Secrets', 'Harry Potter and the Prisoner of Azkaban', 'Harry Potter and the Goblet of Fire'],
+    'George Orwell': ['1984', 'Animal Farm', 'Coming Up for Air'],
+    'Stephen King': ['It', 'The Shining', 'Carrie', 'Misery'],
+    'Agatha Christie': ['Murder on the Orient Express', 'The Murder of Roger Ackroyd', 'And Then There Were None', 'Death on the Nile']
+}
+
+# Write your code here
+kings_books = authors_books['Stephen King']
+shakspeares_books = authors_books['William Shakespeare']
+
+# Testing
+print(f'William Shakespeare\'s books: {shakspeares_books} \nStephen King\'s books: {kings_books}')
