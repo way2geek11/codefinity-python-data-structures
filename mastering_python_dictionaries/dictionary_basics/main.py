0e866c83569d3ee718c5d74f530d79b3de35c6d2
way2geek11 <>
2026-06-06 13:08:35 +0000

    Dictionary Basics

---
+++ mastering_python_dictionaries/dictionary_basics/main.py
@@ -0,0 +1,5 @@
+# Write your code here
+library = {'The Great Gatsby': "F. Scott Fitzgerald", '1984': 'George Orwell', 'To Kill a Mockingbird':'Harper Lee'}
+
+# Testing
+print('Library dictionary:', library)
