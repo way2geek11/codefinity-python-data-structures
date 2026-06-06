a18ba8ca85e939b0106be1743cceafe04db47db1
way2geek11 <>
2026-06-06 13:02:15 +0000

    Clearing dictionaries

---
+++ mastering_python_dictionaries/clearing_dictionaries/main.py
@@ -0,0 +1,13 @@
+authors_books = {
+    'William Shakespeare': ['Hamlet', 'Macbeth', 'Romeo and Juliet', 'Othello'],
+    'J.K. Rowling': ['Harry Potter and the Sorcerer\'s Stone', 'Harry Potter and the Chamber of Secrets', 'Harry Potter and the Prisoner of Azkaban', 'Harry Potter and the Goblet of Fire'],
+    'George Orwell': ['1984', 'Animal Farm', 'Coming Up for Air'],
+    'Stephen King': ['It', 'The Shining', 'Carrie', 'Misery'],
+    'Agatha Christie': ['Murder on the Orient Express', 'The Murder of Roger Ackroyd', 'And Then There Were None', 'Death on the Nile']
+}
+
+# Write your code here
+authors_books.clear()
+
+# Testing
+print("Updated dictionary:", authors_books)
