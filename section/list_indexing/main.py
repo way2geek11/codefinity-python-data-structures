55e6df3fbfd2d2814df1ff0a28d0139c58ffb6b4
way2geek11 <>
2026-06-16 04:29:03 +0000

    List indexing

---
+++ section/list_indexing/main.py
@@ -0,0 +1,8 @@
+cities = ["Paris", "Tokyo", "New York", "Sydney", "Berlin"]
+
+# Write your code here
+second = cities[1]
+last = cities[-1]
+
+# Testing
+print(f"The second element of the array is: {second}, the last element of the array is {last}")
